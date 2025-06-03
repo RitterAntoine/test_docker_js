a5557bc150e5ae7168f240be791b70f3b45513a5
RitterAntoine <antoine.ritter@viacesi.fr>
2025-06-03 12:08:57 +0200

    feat: css file created

--- app/server.js
+++ app/server.js
@@ -13,6 +13,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
